6734bc27543f170211b482bc9377223006345a02
Mirabellensaft <tanja@transfeld.net>
2017-03-28 11:30:01 +0200

    Exercise 3

---
+++ py3.py
@@ -0,0 +1,34 @@
+# line prints a sentence
+print "I will now count my chickens:"
+
+# line prints "Hens" followed by a math term, that adds and divides
+print "Hens", 25 + 30 / 6
+# line prints "Roosters" followed by a math term, that subtracts, multiplies and division by a number, resulting in the remaining number
+print "Roosters", 100 - 25 * 3 % 4
+
+# prints a sentence
+print "Now I will count the eggs:"
+
+# print a math term using all of the above operations
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# prints a question: the truth of an argument
+print "Is is true that 3 + 2 < 5 - 7?"
+
+# prints the the value of the argument
+print 3 + 2 < 5 - 7
+
+# prints a question and calculates the answer
+print "What is 3 + 2?", 3 + 2
+# prints a question and calculates the answer
+print "What is 5 - 7?", 5 - 7
+
+# prints a sentence
+print "Oh, that's why it's False."
+
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2

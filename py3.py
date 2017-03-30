195e83ff43a140b4ba3f2227f003ca197b7fd8d7
Mirabellensaft <tanja@transfeld.net>
2017-03-30 15:56:43 +0200

    Exercise 3, study drill using floating point numbers 🐝

--- py3.py
+++ py3.py
@@ -1,34 +1,23 @@
-# line prints a sentence
 print "I will now count my chickens:"
 
-# line prints "Hens" followed by a math term, that adds and divides
-print "Hens", 25 + 30 / 6
-# line prints "Roosters" followed by a math term, that subtracts, multiplies and division by a number, resulting in the remaining number
-print "Roosters", 100 - 25 * 3 % 4
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
-# prints a sentence
 print "Now I will count the eggs:"
 
-# print a math term using all of the above operations
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
-# prints a question: the truth of an argument
-print "Is is true that 3 + 2 < 5 - 7?"
+print "Is it true that 3 + 2 < 5 -7?"
 
-# prints the the value of the argument
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
-# prints a question and calculates the answer
-print "What is 3 + 2?", 3 + 2
-# prints a question and calculates the answer
-print "What is 5 - 7?", 5 - 7
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
 
-# prints a sentence
 print "Oh, that's why it's False."
-
 
 print "How about some more."
 
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0

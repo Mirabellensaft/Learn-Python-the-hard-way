11fec1dcb07bf74336499207dbdb4f25b05e63ab
Mirabellensaft <tanja@transfeld.net>
2017-03-30 21:49:19 +0200

    Exercise 5, +Study Drills

--- py5.py
+++ py5.py
@@ -5,10 +5,12 @@ weight = 180 # Ibs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+height2 = height * 2.5 #cm
+weight2 = weight * 0.453592 #kg
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
+print "He's %d inches tall. This equals %d cm." % (height, height2)
+print "He's %d pounds heavy. This equals %d kg." % (weight, weight2)
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His theeth are usually %s depending on the coffee." % teeth
 

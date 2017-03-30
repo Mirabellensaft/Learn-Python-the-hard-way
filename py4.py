3222de231fb2be23c75454679f5c7dffbf03b25b
Mirabellensaft <tanja@transfeld.net>
2017-03-30 18:13:19 +0200

    Exercise 4, with comments added

--- py4.py
+++ py4.py
@@ -1,10 +1,18 @@
+#the number of cars available
 cars = 100
+#the number of passengers that fit into a cars
 space_in_a_car = 4.0
+#the number of drivers available
 drivers = 30
+#the number of passengers, that need to be carpooled
 passengers = 90
+#determines the number of cars that are not driven
 cars_not_driven = cars - drivers
+#determines the number of cars that are driven
 cars_driven = drivers
+#determines the maximum number of passengers that can be carpooled
 carpool_capacity = cars_driven * space_in_a_car
+#determines average number of passengers per car
 average_passengers_per_car = passengers / cars_driven
 
 

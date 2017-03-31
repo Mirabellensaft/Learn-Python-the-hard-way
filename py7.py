daea3edc7d2d051384aab530041a148688aa1309
Mirabellensaft <tanja@transfeld.net>
2017-03-31 21:31:21 +0200

    Exercise 7, +Study Drills

---
+++ py7.py
@@ -0,0 +1,25 @@
+# prints a string
+print "Mary had a little lamb."
+# prints a string with a formatted string
+print "It's fleece was white as %s." % 'snow'
+# prints the string 10 times in a row
+print "." * 10 # what'd that do
+
+# defines variables with strings
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# the comma makes both prints be in one row! more then 80 char in one row is bad style!
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12

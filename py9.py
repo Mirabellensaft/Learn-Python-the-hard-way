733403d658e0627195e3cac91bc1921ffa2eadc7
Mirabellensaft <tanja@transfeld.net>
2017-04-01 22:41:09 +0200

    Exercise 9, +study drills

---
+++ py9.py
@@ -0,0 +1,22 @@
+#Here's some new strange stuff, remember type it exactly
+#variable containing a string that will be printed as it is
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#the \n makes a new line for each item of the string
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+#things inside the double-quotes will all be printed as lines.
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+#Mistakes I make:
+#Typos
+#forgot quotes
+#forgot formatter %
+#forgot \n

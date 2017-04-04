eb7c4a02827da45d95643d101269c3246f6b261a
Mirabellensaft <tanja@transfeld.net>
2017-04-04 13:06:49 +0200

    Exercise 10, +study not complete, open questions

--- py10.py
+++ py10.py
@@ -13,9 +13,20 @@ print persian_cat
 print backlash_cat
 print fat_cat
 
-while True :
-    for i in ["/", "-", "|", "\\", "|"]:
-        print "%s\r" % i ,
+#while True :
+#    for i in ["/", "-", "|", "\\", "|"]:
+#        print "%s\r" % i ,
+
+#An escape sequence is a sequence of characters that does not represent itself
+#when used inside a character or string literal, but is translated into another
+#character or a sequence of characters that may be difficult or impossible to
+#represent directly.
+
+#Study Drill
+print "The name of my cat is %s" % (" my cat \")
+
+
+
 
 #Mistakes I make:
 #Typos

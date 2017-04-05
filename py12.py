7414e747a1f1931e434e020fb135896c6a4f35fe
Mirabellensaft <tanja@transfeld.net>
2017-04-05 14:24:39 +0200

    Exercise 12

---
+++ py12.py
@@ -0,0 +1,18 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
+
+
+
+#Mistakes I make:
+#Typos
+#forgot quotes
+#forgot formatter %
+#forgot \n
+
+#things to remember:
+    # go back to study drills of ex 10

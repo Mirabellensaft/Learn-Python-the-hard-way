8a2c26c577538e4d23b74deeb59fd247eed169f5
Mirabellensaft <tanja@transfeld.net>
2017-04-05 11:48:24 +0200

    Exercise 11

---
+++ py11.py
@@ -0,0 +1,25 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?"
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
+
+#An escape sequence is a sequence of characters that does not represent itself
+#when used inside a character or string literal, but is translated into another
+#character or a sequence of characters that may be difficult or impossible to
+#represent directly.
+
+
+#Mistakes I make:
+#Typos
+#forgot quotes
+#forgot formatter %
+#forgot \n
+
+#things to remember:
+    # go back to study drills of ex 10

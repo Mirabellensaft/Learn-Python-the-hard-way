78536ba4e071923299ee10f470702fe55ae2f133
Mirabellensaft <tanja@transfeld.net>
2017-04-06 13:54:56 +0200

    Exercise 13 and study drills

--- py13.py
+++ py13.py
@@ -3,24 +3,25 @@
 from sys import argv
 
 # unpacks the argv: argv is assigned to four variables
-script, first, second, third = argv
-
+script, first, second, third, fourth = argv
+print "What is your argument?"
+fifth = raw_input()
+# script calls up the name of the file, the other arguments have to be added
+# after the filename. If the wrong number of arguments is provided, the argv
+# won't unpack
 print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+print "This is the end:", fourth
+print "This is your argument: %r " % fifth
 
 
+# Mistakes I make:
+# Typos
+# forgot quotes
+# forgot formatter %
+# forgot \n
 
-
-
-
-
-#Mistakes I make:
-#Typos
-#forgot quotes
-#forgot formatter %
-#forgot \n
-
-#things to remember:
-    # go back to study drills of ex 10
+# things to remember:
+# go back to study drills of ex 10

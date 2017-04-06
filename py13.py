22bf2295e0616e36d06abce9414800925f2ea87e
Mirabellensaft <tanja@transfeld.net>
2017-04-06 13:25:14 +0200

    Exercise 13

---
+++ py13.py
@@ -0,0 +1,26 @@
+# imports a module
+# argv is the argument variable. It holds arguments that are passed to the script
+from sys import argv
+
+# unpacks the argv: argv is assigned to four variables
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+
+
+
+
+
+
+#Mistakes I make:
+#Typos
+#forgot quotes
+#forgot formatter %
+#forgot \n
+
+#things to remember:
+    # go back to study drills of ex 10

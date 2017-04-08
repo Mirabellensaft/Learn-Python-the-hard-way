78412d14518e66dc643962f0b40f6a763cdbeaab
Mirabellensaft <tanja@transfeld.net>
2017-04-08 16:06:32 +0200

    Exercise 17, + Study Drills

--- py17.py
+++ py17.py
@@ -4,11 +4,11 @@ from os.path import exists
 script, from_file, to_file = argv
 
 #from_file is an object that is opened, the content that is read
-# produces a string variable: in_file, which is written in to the out_file.
+# produces a string variable: in_data, which is written in to the out_file.
 # out_file has to be closed, as it is an object.
-in_file = open(from_file, "r").read()
+in_data = open(from_file, "r").read()
 out_file = open(to_file, "w")
-out_file.write(in_file)
+out_file.write(in_data)
 
 out_file.close()
 

af7c533b2ec939b11fd8b90accaf1020a68bf8a0
Mirabellensaft <tanja@transfeld.net>
2017-04-08 16:31:43 +0200

    Exercise 16, + Study Drills

--- py16.py
+++ py16.py
@@ -11,9 +11,6 @@ raw_input("?")
 print "Opening the file..."
 target = open(filename, "w")
 
-print "Truncating the file. Goodbye!"
-target.truncate()
-
 print "Now I'm going to ask you for three lines."
 
 line1 = raw_input("line 1: ")
@@ -22,15 +19,12 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
-
+target.write("%s\n%s\n%s\n" % (line1, line2, line3))
 print "Add finally, we close it."
 target.close()
+
+content = open(filename).read()
+print content
 
 # Mistakes I make:
 # Typos

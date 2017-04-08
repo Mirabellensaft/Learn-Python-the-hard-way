3ac35743d7dd8c7d7d4f9b890551ed91bef18f24
Mirabellensaft <tanja@transfeld.net>
2017-04-08 15:14:20 +0200

    Exercise 17, + Study Drills

---
+++ py17.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+#from_file is an object that is opened, the content that is read
+# produces a string variable: in_file, which is written in to the out_file.
+# out_file has to be closed, as it is an object.
+in_file = open(from_file, "r").read()
+out_file = open(to_file, "w")
+out_file.write(in_file)
+
+out_file.close()
+
+
+# Mistakes I make:
+# Typos
+# forgot quotes
+# forgot formatter %
+# forgot \n
+
+# things to remember:
+# go back to study drills of ex 10

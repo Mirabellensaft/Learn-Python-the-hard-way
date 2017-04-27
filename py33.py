713fc1d8f22f9d878180e94f43d0481ca530790e
Mirabellensaft <tanja@transfeld.net>
2017-04-27 15:27:13 +0200

    Exercise 33

---
+++ py33.py
@@ -0,0 +1,25 @@
+i = 0
+numbers = []
+while i < 6:
+    print "At the top i is %d" % i
+    numbers.append(i)
+
+    i = i + 1
+
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
+
+print "The numbers: "
+
+for num in numbers:
+    print num
+
+    
+# Mistakes I make:
+# Typos
+# forgot quotes
+# forgot formatter %
+# forgot \n
+
+# things to remember:
+# go back to study drills of ex 10
